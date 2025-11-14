3e162654864c8c2a2f162925eaa744293b80452c
Nelson Cadenas <nca1478@yahoo.com>
2025-11-14 14:37:55 -0400

    feat: add components barrel export for easier imports

---
+++ components/index.ts
@@ -0,0 +1,5 @@
+export { Card } from "./Card";
+export type { CardProps } from "./Card";
+export { Navbar } from "./Navbar";
+export { Footer } from "./Footer";
+export { ProductCard } from "./ProductCard";
