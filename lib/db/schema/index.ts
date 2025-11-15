603e5c3ce8c24e1507433635b1c74ad1131459fc
Nelson Cadenas <nca1478@yahoo.com>
2025-11-15 18:58:02 -0400

    refactor: reorganize schema exports and remove old product schema

--- lib/db/schema/index.ts
+++ lib/db/schema/index.ts
@@ -1,6 +1,22 @@
+// Auth schemas (better-auth)
 export * from './user';
-export * from './session';
 export * from './account';
-export * from './verification';
+export * from './session';
 export * from './guest';
-export * from './product';
+export * from './verification';
+
+// Core schemas
+export * from './addresses';
+export * from './brands';
+export * from './categories';
+export * from './collections';
+export * from './products';
+export * from './carts';
+export * from './orders';
+export * from './coupons';
+export * from './wishlists';
+
+// Filter schemas
+export * from './filters/genders';
+export * from './filters/colors';
+export * from './filters/sizes';
