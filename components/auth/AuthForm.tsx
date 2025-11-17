b1b18096c486fee17361751d903227c3612b6508
Nelson Cadenas <nca1478@yahoo.com>
2025-11-17 18:16:03 -0400

    added: react-hot-toast para mostrar errores

--- components/auth/AuthForm.tsx
+++ components/auth/AuthForm.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 import { Eye, EyeOff } from 'lucide-react';
 import { useRouter } from 'next/navigation';
+import toast from 'react-hot-toast';
 
 interface AuthFormProps {
     type: 'sign-in' | 'sign-up';
@@ -37,9 +38,14 @@ export default function AuthForm({ type, onSubmit }: AuthFormProps) {
                 ...(isSignUp && { name }),
             });
 
-            if (result?.success) router.push('/');
+            if (result?.success) {
+                router.push('/');
+            } else if (result?.error) {
+                toast.error(result.error);
+            }
         } catch (e) {
             console.log('error', e);
+            toast.error('An unexpected error occurred. Please try again.');
         }
     };
 
