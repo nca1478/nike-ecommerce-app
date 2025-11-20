ec877c1faae9b4b3de27deae9498bfaa1c852797
Nelson Cadenas <nca1478@yahoo.com>
2025-11-20 18:07:25 -0400

    feat: clear search input on blur

--- components/Shared/Navbar.tsx
+++ components/Shared/Navbar.tsx
@@ -134,6 +134,7 @@ export function Navbar() {
                                         onChange={(e) =>
                                             setSearchQuery(e.target.value)
                                         }
+                                        onBlur={() => setSearchQuery('')}
                                         placeholder="Search"
                                         className="w-44 lg:w-52 pl-10 pr-4 py-2 bg-light-200 rounded-full text-sm text-dark-900 placeholder-dark-500 focus:outline-none focus:ring-2 focus:ring-dark-900 transition-all"
                                     />
@@ -191,6 +192,7 @@ export function Navbar() {
                                 type="text"
                                 value={searchQuery}
                                 onChange={(e) => setSearchQuery(e.target.value)}
+                                onBlur={() => setSearchQuery('')}
                                 placeholder="Search"
                                 className="w-full pl-10 pr-4 py-2 bg-light-200 rounded-full text-sm text-dark-900 placeholder-dark-500 focus:outline-none focus:ring-2 focus:ring-dark-900"
                             />
